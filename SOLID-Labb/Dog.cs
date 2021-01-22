fac4423741d39debd1341750fa40774bdc568995
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-01-22 15:45:09 +0100

    the version based on SRP

---
+++ SOLID-Labb/Dog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Labb
+{
+    class Dog : Animal
+    {
+        // same as line 31. see line 31. 
+        // string _owner;
+
+        public Dog(string color): base(color)
+        {
+            
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Dog is barking!");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Dog is sleeping!");
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Dog is eating. " + _owner +" is happy!");
+        }
+        // All the animals have the ability to get an owner so this method shodn be in class "Animal". 
+        // public void TransferOwnership(string owner)
+        // {
+        //    _owner = owner;
+        // }
+    }
+}
